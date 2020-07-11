cbc246a48c0e041d05f45d06e1f9c7b68d3c5fc2
xi <xisun1990@gmail.com>
2020-07-11 01:00:02 -0700

    pause every 10 min / 14 posts

--- WeiboAutoCommentTool.js
+++ WeiboAutoCommentTool.js
@@ -1,5 +1,7 @@
 var weiboAutoCommentTool = (function(){
 	var running = false;
+	var runningInterval = false;
+	var commentNumber = 0;
 	var defaultOption = {
 		//评论间隔时间
 		delay: 10000,
@@ -10,23 +12,23 @@ var weiboAutoCommentTool = (function(){
 	};
 	var needCommentIdList = [];
 	var commentedIdList = [];
-	var dateFormat = function (fmt,date){ 
-		var o = {   
-			"M+" : date.getMonth()+1,                 //月份   
-			"d+" : date.getDate(),                    //日   
-			"h+" : date.getHours(),                   //小时   
-			"m+" : date.getMinutes(),                 //分   
-			"s+" : date.getSeconds(),                 //秒   
-			"q+" : Math.floor((date.getMonth()+3)/3), //季度   
-			"S"  : date.getMilliseconds()             //毫秒   
-		};   
-		if(/(y+)/.test(fmt))   
-			fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));   
-		for(var k in o)   
-			if(new RegExp("("+ k +")").test(fmt))   
-				fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
-		return fmt;   
-	} 
+	var dateFormat = function (fmt,date){
+		var o = {
+			"M+" : date.getMonth()+1,                 //月份
+			"d+" : date.getDate(),                    //日
+			"h+" : date.getHours(),                   //小时
+			"m+" : date.getMinutes(),                 //分
+			"s+" : date.getSeconds(),                 //秒
+			"q+" : Math.floor((date.getMonth()+3)/3), //季度
+			"S"  : date.getMilliseconds()             //毫秒
+		};
+		if(/(y+)/.test(fmt))
+			fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));
+		for(var k in o)
+			if(new RegExp("("+ k +")").test(fmt))
+				fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+		return fmt;
+	}
 	var logger = function(msg){
 		console.log(dateFormat('yyyy-MM-dd hh:mm:ss', new Date()) + ": " + msg);
 	};
@@ -34,6 +36,16 @@ var weiboAutoCommentTool = (function(){
 	var commentButtonClicked = function (commentButton) {
     return commentButton.getAttribute('suda-data')
       .match(/key=(.*)&value=/)[1].indexOf("off") !== -1
+  };
+
+  var isThisItemCommented = function(id){
+    return commentedIdList.indexOf(id) !== -1;
+  };
+
+  var getAllFeedIds = function(){
+    return [...document.querySelectorAll('#pl_feedlist_index .card-wrap')]
+      .map(c => c.getAttribute('mid'))
+      .filter(id => id !== null && !isThisItemCommented(id));
   };
 
 	var comment = function(id){
@@ -51,11 +63,11 @@ var weiboAutoCommentTool = (function(){
 		setTimeout(function(){
 			var textArea = feedItem.querySelector('textarea[action-type="check"]');
 			textArea.value = defaultOption.content;
-			
+
 			var sendButton = feedItem.querySelector('.s-btn-a');
 			sendButton.click();
 			logger("Sending comment content completed.");
-			
+
 			//折叠起来
 			commentButton.click();
 			commentedIdList.push(id);
@@ -64,38 +76,45 @@ var weiboAutoCommentTool = (function(){
 	};
 	var commentThread = function(){
 		logger("Comment thread started.");
-		
 		var innerAction = function(){
+      if(!runningInterval) {
+        logger("forcefully stopped commentThread due to stop task");
+        return;
+      }
+      if(!running) {
+        logger("stopped commentThread due to reaching quota");
+        return;
+      }
 			var id = needCommentIdList.shift();
-			if(id) {
+
+      // avoid being throttled
+			if(id && commentNumber < 14) {
 				comment(id);
-			} else {
+				commentNumber ++;
+			} else if (!id) {
 				logger("WARNING: No feed to process...");
-			}
+			} else {
+			  running = false;
+      }
 			setTimeout(function(){
-				if(!running) {
-					logger("Comment thread action stoped, exit.");
-					return;
-				}
 				innerAction();
 			}, defaultOption.delay);
 		};
 		innerAction();
 	};
-	var isThisItemCommented = function(id){
-		return needCommentIdList.indexOf(id) >= 0;
-	};
-	var getAllFeedIds = function(){
-		return [...document.querySelectorAll('#pl_feedlist_index .card-wrap')]
-      .map(c => c.getAttribute('mid'))
-      .filter(id => id !== null && !isThisItemCommented(id));
-	};
 	var commentAction = function(){
 		if(!running) {
-			logger("Comment action stoped, exit.");
+      logger("stopped commentAction due to reaching quota");
 			return;
 		}
+    if(!runningInterval) {
+      logger("forcefully stopped commentAction due to stop task");
+      return;
+    }
 		var allFeeds = getAllFeedIds();
+    logger("allFeeds: ");
+    console.log(allFeeds);
+
 		//如果没有记录了，则需要滚动屏幕
 		if(!allFeeds.length) {
 			document.body.scrollTop = document.body.scrollTop + 500;
@@ -112,35 +131,56 @@ var weiboAutoCommentTool = (function(){
 			commentAction();
 		}, 20 * 1000);
 	};
+
+	var runTask = function (op) {
+    needCommentIdList = [];
+    commentNumber = 0;
+    logger("WeiboAutoCommentTool start running...");
+    if(typeof(op) !== "undefined") {
+      defaultOption.delay = op.delay || defaultOption.delay;
+      defaultOption.content = op.content || defaultOption.content;
+      logger("Use option: " + JSON.stringify(defaultOption));
+    } else {
+      logger("Use default option: " + JSON.stringify(defaultOption));
+    }
+    logger("WeiboAutoCommentTool running now.");
+    running = true;
+    commentAction();
+    commentThread();
+  };
+
+	// pause posting every 10 min (a little bit over 10 min)
 	this.start = function(op){
-		needCommentIdList = [];
-		commentedIdList = [];
-		logger("WeiboAutoCommentTool start running...");
-		if(typeof(op) !== "undefined") {
-			defaultOption.delay = op.delay || defaultOption.delay;
-			defaultOption.content = op.content || defaultOption.content;
-			
-			logger("Use option: " + JSON.stringify(defaultOption));
-		} else {
-			logger("Use default option: " + JSON.stringify(defaultOption));
-		}
-		
-		logger("WeiboAutoCommentTool running now.");
-		running = true;
-		commentAction();
-		commentThread();
-	};	
+    runningInterval = true;
+    logger('run task ' + new Date());
+    runTask(op);
+    logger("needCommentIdList: ");
+    console.log(needCommentIdList);
+    logger("Commented list: ");
+    console.log(commentedIdList);
+
+		setTimeout(function () {
+      if (!runningInterval) {
+        logger('task has been cancelled');
+        return;
+      }
+		  start();
+    }, 10*61*1000)
+	};
+
 	this.stop = function(){
+	  runningInterval = false;
 		running = false;
 		needCommentIdList = [];
 		logger("User stoped");
-	};	
+	};
+
 	this.stat = function(){
 		logger("Comment queue: ");
 		console.log(needCommentIdList);
 		logger("Commented list: ");
 		console.log(commentedIdList);
 		logger("STAT: Pending comment " + needCommentIdList.length + ", Total commented " + commentedIdList.length);
-	};	
+	};
 	return this;
 })();
